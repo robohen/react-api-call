23582d0758cba77529536edde9058fa454718829
robohen <88636103+robohen@users.noreply.github.com>
2022-10-12 11:03:03 -0700

    this was done over 2 hours and the mistakes were not reported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useState, useEffect} from "React";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  //state
+  const [news, setNews] =useState([]);
+  const[searchQuery, setSearchQuery] =useState('react');
+  const [url, setUrl] =useState('https://hn.algolia.com/api/v1/search?query=$react');
+  // initially it is set to false so it can be manipulated in the app somewhere
+  const [loading, setLoading] = useState(false);
+
+  //fetch news
+  const fetchNews = () => {
+    //this sets the loading to true
+    setLoading(true)
+    fetch(url)
+    .then(result => result.json())
+    //.then(data => console.log(data));
+    .then(data=> setNews(data.hits), setLoading(false))
+    .catch(error => console.log(error));
+
+  };
+  //runs when component mounts and it is changed
+  //after modifications it only changes after the button is pressed instead of real time
+  useEffect(() => {
+    fetchNews();
+  }, [url]);
+
+  const handleChange = (e) =>{
+    setSearchQuery()
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
+
+
+  return(
+    <div>
+      <h2>News</h2>
+      {loading ? <h2>Loading...</h2> : ""}
+      <form onSubmit={handleSubmit}>
+        <input
+        type ="text"
+        value={searchQuery}
+        onChange={handleChange}
+        />
+      </form>
+      {news.map((n,i)=> (
+        <p key={i}>{n.title}</p>
+      ))}
     </div>
   );
-}
-
-export default App;
+};
